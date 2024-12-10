6733d777b6fc7960d783f1c9ab6038cbc3488c18
Houston <houypee@gmail.com>
2024-12-11 08:46:10 +1100

    update plugins

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,11 +1,7 @@
--- autopairs
--- https://github.com/windwp/nvim-autopairs
-
 return {
   'windwp/nvim-autopairs',
-  enabled = false,
+  enabled = true,
   event = 'InsertEnter',
-  -- Optional dependency
   dependencies = { 'hrsh7th/nvim-cmp' },
   config = function()
     require('nvim-autopairs').setup {}
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -12,12 +12,13 @@ return { -- Autoformat
       desc = '[F]ormat buffer',
     },
   },
+  -- This will provide type hinting with LuaLS
+  ---@module "conform"
+  ---@type conform.setupOpts
   opts = {
     notify_on_error = false,
     format_on_save = function(bufnr)
-      -- Disable "format_on_save lsp_fallback" for languages that don't
-      -- have a well standardized coding style. You can add additional
-      -- languages here or re-enable it for the disabled ones.
+      -- format on save for all languages except c, cpp
       local disable_filetypes = { c = true, cpp = true }
       local lsp_format_opt
       if disable_filetypes[vim.bo[bufnr].filetype] then
@@ -32,11 +33,8 @@ return { -- Autoformat
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
-      -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
-      --
-      -- You can use 'stop_after_first' to run the first available formatter from the list
-      -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      python = { 'isort', 'black' },
+      javascript = { 'prettierd', 'prettier', stop_after_first = true },
     },
   },
 }
